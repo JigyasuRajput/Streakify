cb862595291cbbc65d255152d16a82699bebb87b
Jigyasu Rajput <jigyasu2021@gmail.com>
2024-12-01 15:14:52 +0530

    UI improvements

--- src/components/MonthLabels.tsx
+++ src/components/MonthLabels.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { format } from 'date-fns';
+import React from "react";
+import { format } from "date-fns";
 
 interface MonthLabelsProps {
   weeks: Date[][];
@@ -7,10 +7,10 @@ interface MonthLabelsProps {
 
 const MonthLabels: React.FC<MonthLabelsProps> = ({ weeks }) => {
   const months: { label: string; index: number }[] = [];
-  
+
   weeks.forEach((week, index) => {
-    const monthLabel = format(week[0], 'MMM');
-    if (index === 0 || monthLabel !== format(weeks[index - 1][0], 'MMM')) {
+    const monthLabel = format(week[0], "MMM");
+    if (index === 0 || monthLabel !== format(weeks[index - 1][0], "MMM")) {
       months.push({ label: monthLabel, index });
     }
   });
@@ -18,10 +18,11 @@ const MonthLabels: React.FC<MonthLabelsProps> = ({ weeks }) => {
   return (
     <div className="flex text-xs text-gray-400 h-8 relative">
       {months.map(({ label, index }, i) => {
-        const width = i === months.length - 1
-          ? '100%'
-          : `${(months[i + 1].index - index) * 16}px`;
-        
+        const width =
+          i === months.length - 1
+            ? "calc(100% - 16px)"
+            : `${(months[i + 1].index - index) * 16}px`;
+
         return (
           <div
             key={`${label}-${index}`}
